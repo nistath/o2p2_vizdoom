b967757944227bfa6f1d3640a72b391c5a32de1c
Nick Stathas <nistath@mit.edu>
2020-04-29 20:52:31 -0400

    Start doing experiments

--- src/train.py
+++ src/train.py
@@ -4,6 +4,7 @@ import torchvision.models as models
 from torchvision.utils import make_grid
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
+import numpy as np
 import random
 from tqdm import tqdm, trange
 from matplotlib import pyplot as plt
@@ -16,10 +17,16 @@ from models.loss import LossNetwork, masked_mse_loss
 from models.perception import *
 from PerceptualSimilarity.models import PerceptualLoss
 
+
 def idx_label(idx):
     return idx[-1]
 
+
 if __name__ == '__main__':
+    torch.manual_seed(6969)
+    random.seed(6969)
+    np.random.seed(6969)
+
     use_gpu = torch.cuda.is_available()
     device = torch.device("cuda:0" if use_gpu else "cpu")
     # device = torch.device("cpu")
@@ -30,10 +37,12 @@ if __name__ == '__main__':
     img_size = (240, 320)
     dataset = DoomSegmentedDataset('/home/nistath/Desktop/run1/states.npz',
                                    '/home/nistath/Desktop/run1/images/', desired_size=img_size,
-                                #    blacklist=(0, 1,)
+                                   #    blacklist=(0, 1,)
                                    )
 
-    num_features = 333
+    use_perceptual_loss = True
+    reuse_split = True
+    num_features = 256
 
     split = 0.9
     batch_size = 32
@@ -44,13 +53,16 @@ if __name__ == '__main__':
     ).to(device)
 
     if True:
-        all_idxs = dataset.get_all_idxs()
-        random.shuffle(all_idxs)
-        split_point = int(split * len(all_idxs))
-        trn_idxs = all_idxs[:split_point]
-        val_idxs = all_idxs[split_point:]
-
-        del all_idxs
+        if reuse_split:
+            trn_idxs = torch.load('trn_idxs.pth')
+            val_idxs = torch.load('val_idxs.pth')
+        else:
+            all_idxs = dataset.get_all_idxs()
+            random.shuffle(all_idxs)
+            split_point = int(split * len(all_idxs))
+            trn_idxs = all_idxs[:split_point]
+            val_idxs = all_idxs[split_point:]
+            del all_idxs
 
         trn_sampler = StratifiedRandomSampler(trn_idxs, idx_label)
         # trn_sampler = SubsetRandomSampler(trn_idxs)
@@ -58,15 +70,17 @@ if __name__ == '__main__':
             dataset, batch_size=batch_size, num_workers=4, sampler=trn_sampler)
 
         opt = torch.optim.Adam(model.parameters(), lr=1e-3)
-        perceptual_loss_fn = PerceptualLoss(
-            model='net-lin', net='squeeze', use_gpu=use_gpu)
+        if use_perceptual_loss:
+            perceptual_loss_fn = PerceptualLoss(
+                model='net-lin', net='squeeze', use_gpu=use_gpu)
+        mse_loss = torch.nn.MSELoss()
 
         model.train()
         print('Starting training.')
-        max_epoch = 2
+        max_epoch = 4
         for epoch in trange(max_epoch):
-            # focus = [0.1, 0.5, 1, 1.5, 10, 1, 0.8][epoch]
-            focus = 1.0
+            # focus = 1
+            focus = [0.1, 0.5, 1, 1.5][epoch]
             desc = f'focus={focus}'
             for imgs, masks in tqdm(trn_dataloader, desc=desc):
                 imgs = imgs.to(device)
@@ -74,33 +88,37 @@ if __name__ == '__main__':
 
                 imgs_hat = model(imgs)
 
-                masked_loss = masked_mse_loss(imgs_hat, imgs, masks, focus)
-                perceptual_loss = 20 * perceptual_loss_fn.forward(
-                    imgs_hat, imgs).mean()
-                loss = masked_loss + perceptual_loss
+                loss = masked_mse_loss(imgs_hat, imgs, masks, focus)
+                # loss = mse_loss(imgs_hat, imgs)
+                losses = (loss.data.item(),)
+                if use_perceptual_loss:
+                    perceptual_loss = 20 * perceptual_loss_fn.forward(
+                        imgs_hat, imgs).mean()
+                    loss += perceptual_loss
+                    losses += (perceptual_loss.data.item(),)
+
                 opt.zero_grad()
                 loss.backward()
                 opt.step()
 
-                losses = (masked_loss.data.item(),
-                          perceptual_loss.data.item(),
-                          loss.data.item())
                 tqdm.write(f'Loss: {losses}')
 
         torch.save(model.state_dict(), 'model.pth')
-        torch.save(trn_idxs, 'trn_idxs.pth')
-        torch.save(val_idxs, 'val_idxs.pth')
-        val_sampler = SubsetRandomSampler(val_idxs)
+        if not reuse_split:
+            torch.save(trn_idxs, 'trn_idxs.pth')
+            torch.save(val_idxs, 'val_idxs.pth')
     else:
         model.load_state_dict(torch.load('model.pth'))
         trn_idxs = torch.load('trn_idxs.pth')
         val_idxs = torch.load('val_idxs.pth')
 
-    val_sampler = StratifiedRandomSampler(trn_idxs, idx_label)
-    val_sampler = SequentialSampler(val_sampler.as_unshuffled_list(4*batch_size))
-    # val_sampler = SequentialSampler(val_idxs)
+    cheat = True
+    val_num = 4*batch_size if cheat else None
+    val_sampler = StratifiedRandomSampler(
+        trn_idxs if cheat else val_idxs, idx_label)
+    val_sampler = SequentialSampler(val_sampler.as_unshuffled_list(val_num))
     val_dataloader = DataLoader(
-        dataset, batch_size=32, num_workers=0, sampler=val_sampler)
+        dataset, batch_size=batch_size, num_workers=0, sampler=val_sampler)
 
     if val_path.exists():
         shutil.rmtree(val_path)
